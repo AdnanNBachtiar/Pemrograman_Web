85be36267dd26dab70f8b16c115a106b95321197
AdnanNBachtiar <126306382+AdnanNBachtiar@users.noreply.github.com>
2023-06-24 14:44:26 +0700

    Pertemuan 2

---
+++ HTML & CSS/latihan.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$nilai1 = 10;
+$nilai2 = 7;
+$bobotNilai1 = ($nilai1 * 2) ;
+$bobotNilai2 = ($nilai2 * 8) ;
+$totalNilai = ($bobotNilai1 + $bobotNilai2);
+$hasil = ($nilai1 + $nilai2);
+
+echo "Total Nilai = {$totalNilai} <br>";
+echo "Hasil dari 10 + 7 adalah = {$hasil} <br>";
+
+echo "Nama : Adnan Nuur Bachtiar <br>";
+echo "NIM : 121203003 <br>";
+?>  
+
+
+</body>
+</html>
+
+
+
+
